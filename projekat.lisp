d1ff4e233cb88262006c79b8d98481c254674837
agoskomsija <agostvv@gmail.com>
2020-12-09 15:36:30 +0100

    Dodat inicijalni fajl

---
+++ projekat.lisp
@@ -0,0 +1,38 @@
+;;;;; Vestacka Inteligencija ;;;;;
+;;;;; Prva faza do 20.12.2020.
+;;;;; Druga faza do ...
+;;;;; Treca faza do ...
+;;;;; Cetvrta faza do ...
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+;;;;; Projekat : 3D Four in line ;;;;;
+
+;; Prva faza ;;
+
+;;;; Definisati nacin predstavljanja stanja problema (igre)
+;;;; Napisati funkciju za postavljanje pocetnog stanja na osnovu zadate velicine kocke
+;;;; Napisati funkcije za testiranje kraja igre
+;;;; Omoguciti izbor ko ce da igra prvi (covek ili racunar)
+;;;; Prvi igra uvek Igrac X, a drugi O
+;;;; Implementirati funkcije koje obezbedjuju prikaz proizvoljnog stanja problema(igre)
+;;;; Realizovati funkcije koje na osnovu zadatog poteza, u obliku broj_stubica ili
+;(vrsta, kolona), omogucavaju: proveru da li je potez valjan, ako jeste, promenu 
+;prosledjenog stanja problema(igre) odigravanjem poteza
+
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;; KOD ;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+
+
+
+
+
+
+
+
