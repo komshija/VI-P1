4355a07f3c90a82d0de518d48c542bd84816206e
agoskomsija <agostvv@gmail.com>
2020-12-16 21:35:25 +0100

    Dodati komentari za sve funkcije

--- projekat.lisp
+++ projekat.lisp
@@ -25,48 +25,59 @@
 ;;;;;;;;;;;;;; KOD ;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; Predstavljanje problema
-;; ili lista listi, ili velika lista
-
-;; ((- - - -) (- - - -) (- - - -) (- - - -) ... (- - - -))
-;; (- - - - - - - - ... - - - -)
-;; Zavisi kolko ima od N, N min 4, reko da napravimo iz za 6
+;; Globalne promenjive
+; Nstubica : ukupan broj stubica
+; stanjeigre : trenutno realno stanje igre
 
 ;;;;; =========================================================================== ;;;;;
 
 
-;; definisati kako se vraca lista koja je definisana za predstavljanje problema
-;u zavisnosti od N
+;; 1. Definisati kako se vraca lista koja je definisana za predstavljanje problema u zavisnosti od N
 
+;; Funkcija koja inicijalizuje stanje i ukuban broj stubica
+;Param: 
+;N : koliko poteza mogu da stanu na jednom stubicu
 
 (defun init-stanje (N)
   (let () 
     (setq Nstubica (* N N))
-    (setq stanjeigre (init-stanje-pom N (* N N)))
+    (setq stanjeigre (init-stanje-pom (* N N)))
     ))
 
-(defun init-stanje-pom (N pom)
+;; Funkcija koja spaja sve stubice u jednu listu
+;Param:
+;N : koliko imamo ukupno stubica
+;pom : pomocna koja broji do 0
+
+(defun init-stanje-pom (pom)
   (cond 
    ((equalp 0 pom) '())
-   (t (cons (stubic N) (init-stanje-pom N (1- pom))))
-  )))
+   (t (cons (stubic (sqrt Nstubica)) (init-stanje-pom (1- pom))))
+  ))
   
+;; Funkcija koja vraca prazan stubic visine N
+; N : visina stubica
+
 (defun stubic (N)
   (cond
    ((equalp 0 N) ())
    (t (cons '- (stubic (1- N))))
    ))
 
-(init-stanje 4)
-(init-stanje 6)
-(print stanjeigre)
+
+;; Test primeri
+;(init-stanje 4)
+;(init-stanje 6)
+
 
 ;;;;; =========================================================================== ;;;;;
 
 
-;; Testiranje kraja igre
-;; treba prebroji ko ima vise od 4 spojena po dijagonale vertikale, horizontale
-;u svim pravcima
+;; 2. Testiranje kraja igre
+
+;; Proverava da li je cela tabla popunjena
+;Params:
+;tstanje : trenutno stanje igre
 
 (defun krajp (tstanje)
   (cond
@@ -76,17 +87,22 @@
    ))
 
 
-(krajp '((O O X X) (X O X X))); => T
-(krajp '((O O X X) (X O - X))); => NIL
+;; Test primeri
+;(krajp '((O O X X) (X O X X))); => T
+;(krajp '((O O X X) (X O - X))); => NIL
 
 
 ;;;;; =========================================================================== ;;;;;
 
 
-;; treba da pokrene igru, i da namesti ko igra prvi
+;; 3. Treba da pokrene igru, i da namesti ko igra prvi
 ;; ko igra prvi uvek je x, ko igra drugi uvek je o
 
 
+;; Pokrece igru, korisnik bira ko igra prvi, 
+;takodje bira velicinu table sve dok ne unese kako treba
+;Params:
+;
 (defun start-igra ()
   (let* 
       ((prvi (progn (format t "unesite da li covek igra prvi ['t / ()] : ") (read)))
@@ -95,8 +111,9 @@
     
   ))
 
-
-
+;Unos velicine table tj visine stubica
+;Params:
+;
 (defun unesite-N () 
   (let* 
       ((N (progn (format t "unesite koliko je velika tabla [paran broj; min 4] : ") (read) )))
@@ -105,39 +122,44 @@
      ((< N 4) (unesite-N))
      (t N))))
 
-(unesite-N)
+
+;Test primeri
+;(start-igra)
+;(unesite-N)
 
 ;;;;; =========================================================================== ;;;;;
 
 
-;; treba da prikaze proizvoljno stanje
+;; 4. Treba da prikaze proizvoljno stanje
 
-;postavlja string kao pomoc za print
+;;Postavlja string kao pomoc za print
+; Sluzi pri generisanju pomocnih linija pri prikazu
 (setq chars-print "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
-;sklanja sa potez sa vrha
+;;Sklanja sve potez sa vrha svakog stubica
+;Params:
+;tstanje : trenutno stanje igre
 (defun remove-top (tstanje)
   (mapcar #'reverse (mapcar #'cdr (mapcar #'reverse tstanje))))
 
-;vraca potez sa vrha
+;;Vraca poteze sa vrha svakog stubica
+;Params:
+;tstanje : trenutno stanje igre 
 (defun get-top (tstanje)
   (mapcar #'car (mapcar #'reverse tstanje)))
 
-(remove-top '((x o x o) (x x x x) (x - - -) (x o - -) (x x x -) (x - - -)))
-(get-top '((x o x o) (x x x x) (x - - -) (x o - -) (x x x -) (x - - -) (x o x o) (x x x x) (x - - -) (x o - -) (x x x -) (x - - -) (x x x x) (x - - -) (x o - -) (x x x -)))
-
-
-
+;;Vraca listu sa pomocnim strinogivma u zavisnosti od velicine igre
+;Params:
+;N : ukupan broj stubica na tabli
 (defun get-support-chars (N)
   (cond 
    ((equalp 0 N) ())
    (t (append (get-support-chars (1- N)) (list (char chars-print (1- N)))))))
-(char chars-print 3)
 
 
-
-(format t "~%~A" (get-support-chars 16))
-
+;; Vraca listu sa spejs karakterima
+;Params
+;N : duzina liste koja ce biti generisana
 
 (defun space-list (N)
   (cond 
@@ -145,9 +167,10 @@
    (t (cons '#\Space (space-list (1- N))))
    ))
 
-
-
-
+;; Pretvara stubic u stanje sa spejsovima, pogodno za print
+;Params
+;lista : proizvoljno stanje u igri
+;n : pomocna !!! Pozvati sa 0
 (defun space-stanje (lista n)
   (cond
    ((null lista) '())
@@ -158,6 +181,11 @@
        (space-stanje (cdr lista) (mod (1+ n) (sqrt Nstubica))))
    )))
 
+;; pomocna funkcija za celokupan print
+; pretvara iz horizontalnog u vetrikalni format
+;Params:
+;tstanje : trenutno stanje
+;N : ukupan broj stubica 
 
 (defun print-stanje (tstanje N)
   (cond 
@@ -170,73 +198,91 @@
           ))
   ))
 
-
-(defun print-glavna (tstanje N)
+;; Print sa pomocnim linijama
+;Params:
+;tstanje : trenutno stanje igre
+(defun print-glavna (tstanje)
   (let ()
-    (format t "~%~A" (get-support-chars N))
-    (print-stanje tstanje N)
-    (format t "~%~A" (get-support-chars N))
+    (format t "~%~A" (get-support-chars Nstubica))
+    (print-stanje (space-stanje tstanje 0) Nstubica)
+    (format t "~%~A" (get-support-chars Nstubica))
     ))
 
 
 
-(print-glavna (space-stanje stanjeigre 0) Nstubica)
+;;Test primeri
+;(format t "~%~A" (get-support-chars 16))
+;(remove-top '((x o x o) (x x x x) (x - - -) (x o - -) (x x x -) (x - - -)))
+;(get-top '((x o x o) (x x x x) (x - - -) (x o - -) (x x x -) (x - - -) (x o x o) (x x x x) (x - - -) (x o - -) (x x x -) (x - - -) (x x x x) (x - - -) (x o - -) (x x x -)))
+;(print-stanje '((x o x o) (x x x x) (x - - -) (x o - -) (x x x -) (x - - -)) 6)
 
-(print-stanje '((x o x o) (x x x x) (x - - -) (x o - -) (x x x -) (x - - -)) 6)
+;(print-glavna stanjeigre)
 
 
 ;;;;; =========================================================================== ;;;;;
 
 
-;; proverava da li je potez validan 
+;; 5. Proverava da li je potez validan 
+
+;; Proverava da li je potez validan
+;Params:
+;stanje : trenutno stanje
+;broj-stubica : broj stubica koji se proverava
 
 (defun validanp (stanje broj-stubica)
-  
   (cond
    ((null broj-stubica) '())
    ((> broj-stubica Nstubica) '())
    ((< broj-stubica 0) '())
-   (t (stubicp (nth broj-stubica stanje)))
-   )
-  )
+   (t (stubicp (nth broj-stubica stanje)))))
 
+;;Proverava da li je stubic validan
+;Params:
+;lista : stubic sa potezima
 (defun stubicp (lista)
   (cond
    ((null lista) '())
    ((equalp (car lista) '-) t)
-   (t (OR '() (stubicp (cdr lista))))
-   )
-  )
+   (t (OR '() (stubicp (cdr lista))))))
 
 
-(validanp '((x o x o) (x x x x) (x - - -)) 2)
-(validanp '((x o x o) (x x x x) (x - - -)) 1)
+;Test primeri
+;(validanp '((x o x o) (x x x x) (x - - -)) 2)
+;(validanp '((x o x o) (x x x x) (x - - -)) 1)
 
 ;;;;; =========================================================================== ;;;;;
 
-;; omogucava da igrac igra
+;; 6. Omogucava da igrac igra
 
+;; Funkcija kojom se vraca novo stanje
+;Params:
+;stanje : trenutno stanje igre
+;igrac : x / o
+;broj-stubica : broj stubica koji se igra
 (defun odigraj (stanje igrac broj-stubica)
-
   (cond
    ((validanp stanje broj-stubica) (izmeni stanje igrac broj-stubica))
-   (t stanje)
-   )
-  )
+   (t stanje)))
 
+;;
+;Params:
+;stanje : trenutno stanje igre
+;igrac : x / o
+;broj-stubica : broj stubica koji se igra
 (defun izmeni (stanje igrac broj-stubica)
   (cond
    ((= 0 broj-stubica) (cons (dodaj (car stanje) igrac) (cdr stanje)))
-   (t (cons (car stanje) (izmeni (cdr stanje) igrac (- broj-stubica 1))))
-   )
-  )
+   (t (cons (car stanje) (izmeni (cdr stanje) igrac (- broj-stubica 1))))))
 
+;;
+;Params:
+;stubic : lista sa potezima za odredjeni stubic
+;igrac : x / o
 (defun dodaj (stubic igrac)
   (cond
    ((equalp (car stubic) '-) (cons igrac (cdr stubic)))
-   (t (cons (car stubic) (dodaj (cdr stubic) igrac)))
-   )
-  )
+   (t (cons (car stubic) (dodaj (cdr stubic) igrac)))))
 
-(odigraj '((x o x o) (x x x x) (x - - -) (x o - -) (x x x -) (x - - -)) 'x 13)
-(odigraj '((x o x o) (x x x x) (x - - -) (x o - -) (x x x -) (x - - -)) 'o 2)
+;Test primeri:
+;(odigraj '((x o x o) (x x x x) (x - - -) (x o - -) (x x x -) (x - - -)) 'x 13)
+;(odigraj '((x o x o) (x x x x) (x - - -) (x o - -) (x x x -) (x - - -)) 'o 2)
